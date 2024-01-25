05108f9abc5a1cb7c28675ac663f0ebbd047a49c
Ajay Kamath <ajayvkamath10@gmail.com>
2024-01-25 15:42:56 +0530

    Add project files.

---
+++ DynamicArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayAssignment
+{
+    public class DynamicArray<T>
+    {
+        private T[] array;
+        private int size;
+        private int capacity;
+
+        public DynamicArray(int capacity)
+        {
+            this.capacity = capacity;
+            this.array = new T[capacity];
+            this.size = 0;
+        }
+
+        public void Add(int index, T item)
+        {
+            if (index >= capacity)
+            {
+                // Resize the array if the index is beyond the current capacity
+                int newCapacity = capacity * 2;
+                T[] newArray = new T[newCapacity];
+                Array.Copy(array, newArray, capacity);
+                array = newArray;
+                capacity = newCapacity;
+            }
+
+            array[index] = item;
+            size++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= size)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+
+                return array[index];
+            }
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+    }
+
+
+}
